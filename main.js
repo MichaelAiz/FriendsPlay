c1d5160ab9666acaf68c9a722470a0fcc21f47a4
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2020-05-04 17:48:57 -0400

    possibly fixed link bug

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ if (navigator.platform.indexOf("Linux")!=-1) OSName="Linux";
 if (navigator.platform.indexOf("iPhone")!=-1) OSName="iOS";
 if (navigator.platform.indexOf("Pike")!=-1) OSName="iOS";
 if (navigator.platform.indexOf("Android")!=-1) OSName="Android";
-const link1= document.querySelector('#game-link-1 > a')
-const link2= document.querySelector('#game-link-2 > a')
-const link3= document.querySelector('#game-link-3 > a')
+const link1= document.querySelector('#game-link-1');
+const link2= document.querySelector('#game-link-2');
+const link3= document.querySelector('#game-link-3');
 alert(link1);
 if (OSName != "Android"){
     link1.setAttribute('href', 'https://www.apple.com/ca/itunes/');
