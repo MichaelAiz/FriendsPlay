a1c8992b456e6f372f594107110f5d29d8aa3d8c
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2020-05-03 23:08:00 -0400

    fixed typo

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ if (navigator.platform.indexOf("Win")!=-1) OSName="Windows";
 if (navigator.platform.indexOf("Mac")!=-1) OSName="MacOS";
 if (navigator.platform.indexOf("X11")!=-1) OSName="Unix";
 if (navigator.platform.indexOf("Linux")!=-1) OSName="Linux";
-if (navigator.platform.indexOf("iphone")!=-1) OSName="iOS";
+if (navigator.platform.indexOf("iPhone")!=-1) OSName="iOS";
 if (navigator.platform.indexOf("Android")!=-1) OSName="Android";
 alert(OSName);
+if( iPhone.test(navigator.userAgent) ) {
+    alert("Androids Rule");
+   } else {
+    alert("Congratulations, you're not using an iPhone");
+   }
