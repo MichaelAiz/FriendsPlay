a3ad970f7574a4c91beafa051966fb325534b1cf
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2020-05-04 17:22:31 -0400

    made links change based on OS

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ if (navigator.platform.indexOf("Linux")!=-1) OSName="Linux";
 if (navigator.platform.indexOf("iPhone")!=-1) OSName="iOS";
 if (navigator.platform.indexOf("Pike")!=-1) OSName="iOS";
 if (navigator.platform.indexOf("Android")!=-1) OSName="Android";
+const link1= document.querySelector('#game-link-1 > a')
+const link2= document.querySelector('#game-link-2 > a')
+const link3= document.querySelector('#game-link-3 > a')
+if (OSName != "Android"){
+    link1.setAttribute('href', 'https://www.apple.com/ca/itunes/');
+    link1.innerHTML="iTunes";
+    link2.setAttribute('href', 'https://www.apple.com/ca/itunes/');
+    link2.innerHTML="iTunes";
+    link3.setAttribute('href', 'https://www.apple.com/ca/itunes/');
+    link3.innerHTML="iTunes";
+}
